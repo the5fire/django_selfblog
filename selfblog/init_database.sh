3547e768d06986d88e569d2e24c7f21736575478
allenhu <thefivefire@gmail.com>
2014-06-01 00:02:49 +0800

    添加初始化数据

--- selfblog/init_database.sh
+++ selfblog/init_database.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 nohup python manage.py syncdb --noinput > /dev/null 2>&1 &
-nohup python manage.py loaddata initial_data.json > /dev/null 2>&1 &
+sleep 5
+nohup python manage.py loaddata dump-auth.json > /dev/null 2>&1 &
+nohup python manage.py loaddata dump-blog.json > /dev/null 2>&1 &
